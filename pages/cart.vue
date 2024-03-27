b2abdc29dbd552ca54dc7528d9cf7f1b5ed18952
Malshi623 <mhsm.dulakshi@gmail.com>
2024-03-27 15:31:00 +0530

    build for PR

---
+++ pages/cart.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h2>Cart</h2>
+    </div>
+  </template>
+  
+  <style scoped>
+    h2 {
+      margin-bottom: 20px;
+      font-size: 36px;
+    }
+    p {
+      margin: 20px 0;
+    }
+  </style>
+  
+  <script setup>
+  
+  </script>
